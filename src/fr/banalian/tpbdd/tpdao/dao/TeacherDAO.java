1777789bbdf8708e6a1cec9b82c1679e6e1f5174
Auraen <aurane.martin@etu.univ-tours.fr>
2021-12-14 15:19:56 +0100

    feat (DAO): added TeacherDAO

---
+++ src/fr/banalian/tpbdd/tpdao/dao/TeacherDAO.java
@@ -0,0 +1,71 @@
+package fr.banalian.tpbdd.tpdao.dao;
+
+import fr.banalian.tpbdd.tpdao.ConnectBdd;
+import fr.banalian.tpbdd.tpdao.model.Teacher;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class TeacherDAO implements DAO<Teacher>{
+
+    /**
+     * get a connection to the database from connectBdd and make a query to get all the teacher
+     */
+    @Override
+    public ArrayList<Teacher> getAll() {
+        ArrayList<Teacher> teacher = new ArrayList<>();
+        Statement stmt = ConnectBdd.getNewStatement();
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery("SELECT * FROM teacher");
+            teacher = iterateThroughResultSet(rs);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return teacher;
+    }
+
+    @Override
+    public Teacher get(int id) {
+        Statement stmt = ConnectBdd.getNewStatement();
+        Teacher teacher = null;
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery("SELECT * FROM teacher WHERE id = "+ id +";");
+            teacher = iterateThroughResultSet(rs).get(0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return teacher;
+    }
+
+    @Override
+    public boolean update(Teacher teacher) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean add(Teacher teacher) {
+        return false;
+    }
+
+    private ArrayList<Teacher> iterateThroughResultSet(ResultSet rs) throws SQLException {
+        ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            String lastName = rs.getString("lastname");
+            String firstName = rs.getString("firstname");
+
+            Teacher teacher = new Teacher(id, lastName, firstName);
+            teachers.add(teacher);
+        }
+        return teachers;
+    }
+}
